e5dd4bda5012c41658df301f4ef95ff2c1fb24aa
cobra-r9 <cobra.rev.9@gmail.com>
2026-06-26 22:33:14 +0530

    modified colors

--- config/bspwm/bspwm.d/sources/colors.sh
+++ config/bspwm/bspwm.d/sources/colors.sh
@@ -3,6 +3,6 @@
 # This file will be sourced for the colors.
 
 active_color="#000000"
-focused_color="#7e9fbe"
-normal_color="#252a34"
+focused_color="#3a4755"
+normal_color="#1e1e1e"
 pfc_color="#343c49"
